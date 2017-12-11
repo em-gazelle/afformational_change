aec1c39269c922ddca7c49d9f9d805d76b330a9a
em-gazelle <emmaf9@gmail.com>
2017-12-10 22:00:29 -0500

    Afformation Enums

---
+++ app/controllers/sent_afformations_controller.rb
@@ -0,0 +1,2 @@
+class SentAfformationsController < ApplicationController
+end
--- app/models/afformation.rb
+++ app/models/afformation.rb
@@ -1,11 +1,9 @@
 class Afformation < ActiveRecord::Base	
-	include AfformationEnum
+	enum afformation_focus_area: { general: 0, career: 1, health: 2, athletic: 3 }
 
 	has_many :users, through: :sent_afformations
 	# applies only to user-submitted afformations
 	belongs_to :user, required: false
-
-	# enum afformation_focus_area: { career: 0, health: 1, athletic: 2, positivity: 3 }
 
 	validates_presence_of :afformation_text
 
