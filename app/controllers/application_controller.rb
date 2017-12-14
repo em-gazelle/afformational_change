daf909ff548625224b97cfa74c73d239bddb0c8a
em-gazelle <emmaf9@gmail.com>
2017-12-13 23:16:05 -0500

    Views Updated to reflect user's array of afformation focus areas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :timezone, :personal_afformation_focus_areas => [] ])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :timezone, :personal_afformation_focus_area => [] ])
 	end
 
 
