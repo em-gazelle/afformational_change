1617290250fa5bae5b22052b4c16b5c9e3ecebea
em-gazelle <emmaf9@gmail.com>
2017-12-21 22:03:12 -0500

    Front-End : Views. Added Profile page for user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def show
+	@user_afformations_count = current_user.afformations.count
+
+	user_sent_afformations = SentAfformation.where(user_id: current_user.id)
+	@user_sent_afformations_count = user_sent_afformations.count
+	@user_responses_count = user_sent_afformations.where.not(user_response_to_afformation: nil).count
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  get 'home/index'
+
+  get 'home/show', as: "profile"
 
   resources :afformations, except: [:show]
-
   resources :sent_afformations, only: [:index, :edit, :update]
 
   root 'sent_afformations#index'
