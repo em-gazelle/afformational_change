23dbf2b66604aaed57ef52d7739d568360b1842b
em-gazelle <emmaf9@gmail.com>
2017-12-23 08:58:55 -0500

    Bug Fix: Emails were downloading inline attachments in GMail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,7 @@
 class UserMailer < ApplicationMailer
 	
 	def welcome_email(user_email)
-		attachments.inline['path_water_sunsetrise.jpeg'] = {content: File.read("#{Rails.root}/app/assets/images/path_water_sunsetrise.jpeg"), content_id: 'logo'}
-		@welcome_picture =  attachments['path_water_sunsetrise.jpeg']
+		attachments.inline['path_water_sunsetrise.jpeg'] = File.read('app/assets/images/path_water_sunsetrise.jpeg').force_encoding('utf-8').encode
 		mail(to: user_email, subject: 'How did you become so open to change?')
 	end
 
@@ -11,8 +10,7 @@ class UserMailer < ApplicationMailer
 		@afformation = afformation
 
 		@random_image = random_image_from_directory
-		attachments.inline[@random_image] = {content: File.read("#{Rails.root}/app/assets/images/#{@random_image}"), content_id: 'logo'}
-		@afformation_picture =  attachments[@random_image]
+		attachments.inline[@random_image] = File.read("app/assets/images/#{@random_image}").force_encoding('utf-8').encode
 		
 		mail(to: @user.email, subject: @afformation.afformation_text)
 		SentAfformation.create!(afformation_id: @afformation.id, user_id: @user.id)
